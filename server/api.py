c147448c95ad3d1414140b88fe76af8e9794c249
Bhumsitt Pramuanpornsatid <32533504+Bosebcc@users.noreply.github.com>
2024-12-07 23:08:42 -0600

    try to fix cors

--- server/api.py
+++ server/api.py
@@ -11,9 +11,9 @@ db_conn = pool.connect()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*', "https://jobkinator.web.app", "http://localhost:3000"],  # Add localhost:3000
+    allow_origins=["https://jobkinator.web.app", "http://localhost:3000"],  # Add localhost:3000
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Explicitly specify allowed methods
+    allow_methods=["*"], 
     allow_headers=["*"]
 )
 
